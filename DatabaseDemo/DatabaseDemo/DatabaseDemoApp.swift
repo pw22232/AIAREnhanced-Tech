23d86aaf526a6990b1b7378a7b271eb174caf2ac
Peter Sheehan <ak22112@bristol.ac.uk>
2024-01-24 19:48:29 +0000

    configures firebase app

---
+++ DatabaseDemo/DatabaseDemo/DatabaseDemoApp.swift
@@ -0,0 +1,32 @@
+//
+//  DatabaseDemoApp.swift
+//  DatabaseDemo
+//
+//  Created by Peter Sheehan on 24/01/2024.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    
+      FirebaseApp.configure()
+      print("firebase configured")
+      
+      return true
+  }
+}
+
+@main
+struct DatabaseDemoApp: App {
+    
+    // register app delegate for Firebase setup
+      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
