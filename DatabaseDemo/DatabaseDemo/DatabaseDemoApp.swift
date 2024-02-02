3e943ac7221b8010d04964cea11613b2a0b693a8
Peter Sheehan <ak22112@bristol.ac.uk>
2024-02-02 15:42:22 +0000

    whatever

--- DatabaseDemo/DatabaseDemo/DatabaseDemoApp.swift
+++ DatabaseDemo/DatabaseDemo/DatabaseDemoApp.swift
@@ -9,14 +9,25 @@ import SwiftUI
 import FirebaseCore
 import FirebaseAuth
 
+
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    
+      
       FirebaseApp.configure()
       print("firebase configured")
       
       Auth.auth().signInAnonymously()
       print("signed in")
+      
+      guard let user = Auth.auth().currentUser else {
+          return false
+      }
+      
+      print(user.isAnonymous)
+      print(user.uid)
+      
+//      user.delete()
+//      print("user deleted")
       
       return true
   }
