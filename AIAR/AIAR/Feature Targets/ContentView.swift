ac9dd2e6cad72f3391e4114e291344e90f62b9b6
zakmansuri <zak.j.mansuri@gmail.com>
2024-02-09 19:27:30 +0000

    Cleaned up the UI, made it look pretty

--- AIAR/AIAR/Feature Targets/ContentView.swift
+++ AIAR/AIAR/Feature Targets/ContentView.swift
@@ -13,7 +13,7 @@ struct VideoPlayerView: UIViewRepresentable {
     
     func makeUIView(context: Context) -> UIView {
         let view = UIView()
-        let player = AVPlayer(url: Bundle.main.url(forResource: "backgroundVideo", withExtension: "mov")!)
+        let player = AVPlayer(url: Bundle.main.url(forResource: "backgroundVideo1", withExtension: "mov")!)
         let playerLayer = AVPlayerLayer(player: player)
         playerLayer.frame = UIScreen.main.bounds
         playerLayer.videoGravity = .resizeAspectFill
@@ -69,31 +69,63 @@ struct ContentView: View {
                 .ignoresSafeArea()
             
             VStack {
-                Text("Welcome to Galasa AR AI")
+                Text("Welcome to")
                     .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .scaleEffect(1.2) // Increase the size by 50%
+                    .shadow(color: Color.black, radius: 3, x: 0, y: 5)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, -220)
+                
+                Text("Galasa AR AI")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .scaleEffect(1.5) // Increase the size by 50%
+                    .shadow(color: Color.black, radius: 3, x: 0, y: 5)
                     .fontWeight(.bold)
                     .foregroundColor(Color.white)
                     .multilineTextAlignment(.center)
+                    .padding(.top, -180)
     
                 Text("Technical Document Enhancer")
-                    .font(.title2)
+                    .font(.title)
+                    .scaleEffect(0.6) // Increase the size by 50%
+                    .shadow(color: Color.black, radius: 3, x: 0, y: 5)
                     .fontWeight(.semibold)
                     .foregroundColor(Color.white)
-                
+                    .padding(.top, -145) // Add bottom padding for space
+
                 NavigationLink(destination: ARACTUAL()) {
                     Text("Go to the Majic World")
+                        .fontWeight(.semibold)
                         .padding()
                         .background(Color.white)
                         .foregroundColor(Color.black)
                         .cornerRadius(8)
+                        .shadow(color: Color.black, radius: 3, x: 0, y: 2)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.black, lineWidth: 1)
+                        )
                 }
+                .padding(.bottom, 10) // Add bottom padding for space
+
                 NavigationLink(destination: TeamRepresentable()) {
                     Text("Developer introduction")
+                        .fontWeight(.semibold)
                         .padding()
                         .background(Color.white)
                         .foregroundColor(Color.black)
                         .cornerRadius(8)
+                        .shadow(color: Color.black, radius: 3, x: 0, y: 2)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.black, lineWidth: 1)
+                        )
                 }
+
+
             }
         }
         .navigationBarTitle("Main Page") // Set the title in the navigation bar
