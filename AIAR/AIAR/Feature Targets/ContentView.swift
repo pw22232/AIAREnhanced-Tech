a972c0d2333d5f7b29d6f8def9800969582ab4f7
Louie Sinadjan <bn22907@bristol.ac.uk>
2024-02-14 12:00:40 +0000

    Fixed button functionality

--- AIAR/AIAR/Feature Targets/ContentView.swift
+++ AIAR/AIAR/Feature Targets/ContentView.swift
@@ -64,69 +64,72 @@ class VideoPlayerViewController: UIViewController {
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VideoPlayerView()
-                .ignoresSafeArea()
-            
-            VStack {
-                Text("Welcome to")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .scaleEffect(1.2) // Increase the size by 50%
-                    .shadow(color: Color.black, radius: 3, x: 0, y: 5)
-                    .foregroundColor(Color.white)
-                    .multilineTextAlignment(.center)
-                    .padding(.top, -220)
+        
+        return NavigationView{
+            ZStack {
+                VideoPlayerView()
+                    .ignoresSafeArea()
+                    .allowsHitTesting(false)
                 
-                Text("Galasa AR AI")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .scaleEffect(1.5) // Increase the size by 50%
-                    .shadow(color: Color.black, radius: 3, x: 0, y: 5)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.white)
-                    .multilineTextAlignment(.center)
-                    .padding(.top, -180)
-    
-                Text("Technical Document Enhancer")
-                    .font(.title)
-                    .scaleEffect(0.6) // Increase the size by 50%
-                    .shadow(color: Color.black, radius: 3, x: 0, y: 5)
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color.white)
-                    .padding(.top, -145) // Add bottom padding for space
-
-                NavigationLink(destination: ARACTUAL()) {
-                    Text("Go to the Majic World")
+                VStack {
+                    Text("Welcome to")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .scaleEffect(1.2) // Increase the size by 50%
+                        .shadow(color: Color.black, radius: 3, x: 0, y: 5)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.top, -220)
+                    
+                    Text("Galasa AR AI")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .scaleEffect(1.5) // Increase the size by 50%
+                        .shadow(color: Color.black, radius: 3, x: 0, y: 5)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.top, -180)
+                    
+                    Text("Technical Document Enhancer")
+                        .font(.title)
+                        .scaleEffect(0.6) // Increase the size by 50%
+                        .shadow(color: Color.black, radius: 3, x: 0, y: 5)
                         .fontWeight(.semibold)
-                        .padding()
-                        .background(Color.white)
-                        .foregroundColor(Color.black)
-                        .cornerRadius(8)
-                        .shadow(color: Color.black, radius: 3, x: 0, y: 2)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.black, lineWidth: 1)
-                        )
+                        .foregroundColor(Color.white)
+                        .padding(.top, -145) // Add bottom padding for space
+                    
+                    NavigationLink(destination: ARACTUAL()) {
+                        Text("Go to the Majic World")
+                            .fontWeight(.semibold)
+                            .padding()
+                            .background(Color.white)
+                            .foregroundColor(Color.black)
+                            .cornerRadius(8)
+                            .shadow(color: Color.black, radius: 3, x: 0, y: 2)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.black, lineWidth: 1)
+                            )
+                    }
+                    .padding(.bottom, 10) // Add bottom padding for space
+                    
+                    NavigationLink(destination: TeamRepresentable()) {
+                        Text("Developer introduction")
+                            .fontWeight(.semibold)
+                            .padding()
+                            .background(Color.white)
+                            .foregroundColor(Color.black)
+                            .cornerRadius(8)
+                            .shadow(color: Color.black, radius: 3, x: 0, y: 2)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.black, lineWidth: 1)
+                            )
+                    }
                 }
-                .padding(.bottom, 10) // Add bottom padding for space
-
-                NavigationLink(destination: TeamRepresentable()) {
-                    Text("Developer introduction")
-                        .fontWeight(.semibold)
-                        .padding()
-                        .background(Color.white)
-                        .foregroundColor(Color.black)
-                        .cornerRadius(8)
-                        .shadow(color: Color.black, radius: 3, x: 0, y: 2)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.black, lineWidth: 1)
-                        )
-                }
-
-
             }
+
         }
         .navigationBarTitle("Main Page") // Set the title in the navigation bar
     }
